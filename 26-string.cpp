58e964b290e909e4fdc29e1b0bbd04860145d3ea
rohit jangid <khatirohit124@gmail.com>
2026-05-22 08:44:13 +0530

    start string

--- 26-string.cpp
+++ 26-string.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+void reverse(char name[], int n){
+    int s = 0;
+    int e = n-1;
+    while(s<e){
+        swap(name[s], name[e]);
+        s++;
+        e--;
+    }
+}
+
 int getLength(char name[]){
     int count = 0;
     for(int i = 0; name[i] != '\0'; i++){
@@ -16,6 +26,15 @@ int main(){
     cout<<"Enter your name: ";
     cin>>name;
 
+    // 1. print char
     cout<<"Your name is: "<<name<<endl;
-    cout<<"Your name length is: "<<getLength(name);
+
+    // 2. print char length
+    int n = getLength(name);
+    cout<<"Your name length is: "<<n<<endl;
+
+    // 3. reverse the char
+    reverse(name, n);
+    cout<<"Reverse char is: "<<name;
+
 }
