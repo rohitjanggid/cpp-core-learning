e0bc94b8976eeda6d7168629c5779284b9b645ce
rohit jangid <khatirohit124@gmail.com>
2026-06-01 21:29:33 +0530

    learn about inline fuctions

---
+++ 36-globalVariable.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+// global variable is a bad practice
+int score = 15;
+
+void a(int& i){
+    cout<<score<<" in a"<<endl;
+    cout<<i<<endl;
+    // b(i);
+
+}
+
+void b(int& i){
+    cout<<score<<" in b"<<endl;
+    cout<<i<<endl;
+
+}
+
+int main() {
+
+    cout<<score<<" in main"<<endl;
+    int i = 5;
+    a(i);
+    b(i);
+
+    return 0;
+}
